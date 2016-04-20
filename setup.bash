f5f215f5bda181f662beae3075e40ca8cccb2daa
Pioneer <pioneer@idsia.ch>
2016-04-20 16:32:28 +0200

    Added nimbro

--- setup.bash
+++ setup.bash
@@ -17,3 +17,6 @@ alias pioneer_localization='docker run -it --rm -v /tmp/env:/env -v $HOST_HOME/c
 
 alias pioneer_control='docker run -it --rm -v /tmp/env:/env -v $HOST_HOME/catkin_ws/src:/home/root/catkin_ws/src:ro \
 --privileged --net=host jeguzzi/pioneer:dev roslaunch pioneer_outdoor indoor_navigation.launch sick:=1'
+
+alias pioneer_network='docker run -it --rm -v /tmp/env:/env -v $HOST_HOME/catkin_ws/src:/home/root/catkin_ws/src:ro \
+--privileged --net=host jeguzzi/pioneer:base roslaunch pioneer_outdoor network.launch nimbro:=1'
