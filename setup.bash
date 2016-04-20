9c602a6e8a1714baa2548bb3b13f40538139c84e
Pioneer <pioneer@idsia.ch>
2016-04-20 18:26:40 +0200

    Added port args

--- setup.bash
+++ setup.bash
@@ -19,4 +19,4 @@ alias pioneer_control='docker run -it --rm -v /tmp/env:/env -v $HOST_HOME/catkin
 --privileged --net=host jeguzzi/pioneer:dev roslaunch pioneer_outdoor indoor_navigation.launch sick:=1'
 
 alias pioneer_network='docker run -it --rm -v /tmp/env:/env -v $HOST_HOME/catkin_ws/src:/home/root/catkin_ws/src:ro \
---privileged --net=host jeguzzi/pioneer:base roslaunch pioneer_outdoor network.launch nimbro:=1'
+--privileged --net=host jeguzzi/pioneer:base roslaunch pioneer_outdoor network.launch nimbro:=1 remote_port:=10002'
