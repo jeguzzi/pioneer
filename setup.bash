3d87f93eb2d9701b250123687862cbdffae66615
Pioneer <pioneer@idsia.ch>
2016-10-20 16:05:03 +0200

    Local changes

--- setup.bash
+++ setup.bash
@@ -3,7 +3,7 @@
 HOST_HOME=`pwd`
 
 
-V="-v /tmp/env:/env -v $HOST_HOME/catkin_ws/src/pioneer_outdoor:/home/root/catkin_ws/src/pioneer_outdoor:ro -v $HOST_HOME/settings/$HOSTNAME.env:/env/settings.env"
+V="-v /tmp/env:/env:ro -v $HOST_HOME/catkin_ws/src/pioneer_outdoor:/home/root/catkin_ws/src/pioneer_outdoor:ro -v $HOST_HOME/settings/$HOSTNAME.env:/env2/settings.env:ro"
 CONFIG="--privileged --net=host $V"
 
 
@@ -28,12 +28,14 @@ alias pioneer_i_localization='docker run -it --rm $CONFIG jeguzzi/pioneer:dev ro
 
 alias pioneer_control='docker run -it --rm $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor outdoor_navigation.launch sick:=1'
 
+alias pioneer_control_odom='docker run -d --name navigation $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor navigation.launch sick:=0'
+
 alias pioneer_network='docker run -it --rm $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor network.launch nimbro:=1'
 
 
 
 alias pioneer_it='docker run -it --rm $CONFIG jeguzzi/pioneer:dev'
-alias pioneer_itm='docker run -it --rm $CONFIG -v /var/run/docker.sock:/var/run/docker.sock jeguzzi/pioneer:monitor'
+alias pioneer_itm='docker run -it --rm $CONFIG -v /var/run/docker.sock:/var/run/docker.sock jeguzzi/pioneer:dev'
 
 
 
@@ -45,10 +47,13 @@ alias pioneer_lcm='docker run -d $CONFIG --name lcm jeguzzi/pioneer:dev roslaunc
 
 alias pioneer_core_core='docker run -d --restart=always --name core  $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor core-core.launch model:=$HOSTNAME'
 alias pioneer_core_robot='docker run -d --restart=always --name p3at  $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor core_robot.launch p2os:=1 --wait'
-alias pioneer_monitor='docker run -d --restart=always --name ui $CONFIG -v /var/run/docker.sock:/var/run/docker.sock jeguzzi/pioneer:monitor roslaunch pioneer_outdoor monitor.launch --wait'
+alias pioneer_monitor='docker run -d --restart=always --name ui $CONFIG -v /var/run/docker.sock:/var/run/docker.sock jeguzzi/pioneer:dev roslaunch pioneer_outdoor monitor.launch --wait'
 
-alias pioneer_video='docker run -d --restart=always --name video_stream $CONFIG -v /var/run/docker.sock:/var/run/docker.sock jeguzzi/pioneer:monitor roslaunch pioneer_outdoor video.launch --wait'
+alias pioneer_video='docker run -d --restart=always --name video_stream $CONFIG -v /var/run/docker.sock:/var/run/docker.sock jeguzzi/pioneer:dev roslaunch pioneer_outdoor video.launch --wait'
 
 alias pioneer_ahrs='docker run -d --restart=always --name ahrs  $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor ahrs-core.launch --wait'
 
 alias pioneer_odom='docker run -d --restart=always --name odom $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor odom_localization.launch --wait'
+alias pioneer_odom_compass='docker run -d --name odom_compass $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor odom_localization.launch compass:=true --wait'
+alias pioneer_loc_idsia='docker run -d  --name localize_idsia $CONFIG jeguzzi/pioneer:dev roslaunch pioneer_outdoor idsia.launch --wait'
+
